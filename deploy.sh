1ee08fb39b3b98dc03853927bda8d6de462fd455
DanielStephens <daniel.stephens02@gmail.com>
2020-10-20 19:33:15 +0100

    Deployed version 2.4.1

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,17 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR"
 
 ./install.sh $1
+
+git tag -a "$1" -m "Deployed version $1" -f
+
 mvn deploy:deploy-file \
       -Dfile=target/ktest.jar \
       -DrepositoryId=clojars \
       -Durl=https://clojars.org/repo \
       -DpomFile=pom.xml
+
+echo "Depending on the state of your branch, you should run"
+echo "git add -u && \\"
+echo "git commit -m 'Deployed version $1' && \\"
+echo "git push && \\"
+echo "git push --tags && \\"
