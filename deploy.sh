91e748089328590f8cf29abde937cec1b26db079
DanielStephens <daniel.stephens02@gmail.com>
2021-11-06 18:11:24 +0000

    Improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR"
 
 ./install.sh $1
+git tag -a "$1" -m "Releasing version $1"
+git push origin --tags
+
 mvn deploy:deploy-file \
       -Dfile=target/ktest.jar \
       -DrepositoryId=clojars \
