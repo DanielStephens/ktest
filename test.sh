4e0b92a9e3a75f6b42a4d6a20b27a8a95c1ff83d
DanielStephens <daniel.stephens02@gmail.com>
2022-09-23 10:19:50 +0100

    Release 2.8.2-alpha1

---
+++ test.sh
@@ -0,0 +1,10 @@
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$DIR"
+
+# update the existing pom with any change in deps.edn dependencies
+clj -Spom
+
+# generate java classes
+clj -M:build
+# run tests
+clj -M:test
